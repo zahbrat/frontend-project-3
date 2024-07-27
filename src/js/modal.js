c437d0e12b9bd1e8c71939b7d66e28fce219169b
lidiia <lidiiadolfin@gmail.com>
2024-07-27 10:53:36 +0300

    feat: add

--- src/js/modal.js
+++ src/js/modal.js
@@ -2,18 +2,29 @@ const modal = document.querySelector('[data-model]')
 const modalOpen = document.querySelector('[data-open-modal]')
 const modalClose = document.querySelector('[data-modal-close]')
 const backdrop = document.querySelector('.backdrop')
+const input = document.querySelector('.footer__input')
 
 console.log(modal);
 console.log(modalOpen);
 console.log(modalClose);
 console.log(backdrop);
+console.log(input);
 
 const toogleModal = () => {
     modal.classList.toggle('is-hidden')
     modal.classList.toggle('no-scroll')
 }
+
+ modalOpen.addEventListener('click', () => {
+    let inputValue = input.value
+    if(inputValue !== '' && inputValue.includes('@') && inputValue.includes('.')) {
+        toogleModal()
+    } else {
+        alert('Пошта введена неправильно 🤨')
+    }
+ })
+
 modalClose.addEventListener('click', toogleModal)
-modalOpen.addEventListener('click', toogleModal)
 backdrop.addEventListener('click', (e) => {
     if (e.target === backdrop) {
         toogleModal();

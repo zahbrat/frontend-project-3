3d4f661f394c3b746dd3aa351a4b41c4e360d510
lidiia <lidiiadolfin@gmail.com>
2024-08-08 10:38:19 +0300

    feat:change

---
+++ src/js/time_cancul.js
@@ -0,0 +1,28 @@
+const inputTime = document.querySelector('.time__wrapper--input')
+const btnTime = document.querySelector('.time__wrapper--btn')
+const resultTime = document.querySelector('.time__days--info')
+console.log(inputTime, btnTime, resultTime);
+
+
+const getTimeFromMinutes = function () {
+console.log('click');
+
+let minutes = +inputTime.value
+  if (
+    typeof minutes !== 'number' ||
+    !Number.isInteger(minutes) ||
+    minutes < 0
+  ) {
+    return 'Помилка, перевірте дані';
+  }
+
+  let days = Math.floor(minutes / 1440)
+  minutes %= 1440
+  let hour = Math.floor(minutes / 60);
+  minutes %= 60;
+  let remainingMinutes = minutes % 60;
+  
+  resultTime.textContent = `${days} дн ${hour}:${remainingMinutes}`
+}
+
+btnTime.addEventListener('click', getTimeFromMinutes)

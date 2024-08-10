76518cb9cae7ad5fb4f13362f66c7decf67a9fbb
lidiia <lidiiadolfin@gmail.com>
2024-08-10 10:39:18 +0300

    feat:change

--- src/js/time_cancul.js
+++ src/js/time_cancul.js
@@ -1,13 +1,12 @@
-const inputTime = document.querySelector('.time__wrapper--input')
-const btnTime = document.querySelector('.time__wrapper--btn')
-const resultTime = document.querySelector('.time__days--info')
+const inputTime = document.querySelector('.time__wrapper--input');
+const btnTime = document.querySelector('.time__wrapper--btn');
+const resultTime = document.querySelector('.time__days--info');
 console.log(inputTime, btnTime, resultTime);
 
-
 const getTimeFromMinutes = function () {
-console.log('click');
+  console.log('click');
 
-let minutes = +inputTime.value
+  let minutes = +inputTime.value;
   if (
     typeof minutes !== 'number' ||
     !Number.isInteger(minutes) ||
@@ -16,14 +15,15 @@ let minutes = +inputTime.value
     return 'Помилка, перевірте дані';
   }
 
-
-  let days = Math.floor(minutes / 1440)
-  minutes %= 1440
+  let days = Math.floor(minutes / 1440);
+  minutes %= 1440;
   let hour = Math.floor(minutes / 60);
   minutes %= 60;
   let remainingMinutes = minutes % 60;
-  
-  resultTime.textContent = `${days} дн ${hour}:${remainingMinutes}`
-}
 
-btnTime.addEventListener('click', getTimeFromMinutes)
+  resultTime.textContent = `${days} дн ${hour
+    .toString()
+    .padStart(2, '0')}:${remainingMinutes.toString().padStart(2, '0')}`;
+};
+
+btnTime.addEventListener('click', getTimeFromMinutes);

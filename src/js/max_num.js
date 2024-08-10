ea7ed6556f7f64e1516d15178bd8172f3826b2c7
lidiia <lidiiadolfin@gmail.com>
2024-08-10 11:33:08 +0300

    feat:change

---
+++ src/js/max_num.js
@@ -0,0 +1,4 @@
+const numInput = document.querySelector('.num__wrapper--input')
+const numWrapper = document.querySelector('.num__wrapper')
+const numResult = document.querySelector('.num__subtitle')
+

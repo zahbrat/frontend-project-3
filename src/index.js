db0fc39dc5919092c537bf2ca21513a85b13315e
Your Name <you@example.com>
2024-08-22 20:32:52 +0300

    feat commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const btnOpenInterakt = document.querySelector('.header__interakt');
-const headerSubmenu = document.querySelector('.header__submenu-items');
+const headerSubmenu = document.querySelector('.header__submenu');
 console.log(btnOpenInterakt);
 console.log(headerSubmenu);
 btnOpenInterakt.addEventListener('click', () => {
@@ -7,3 +7,11 @@ btnOpenInterakt.addEventListener('click', () => {
     
   headerSubmenu.classList.toggle('idk');
 });
+
+let root = document.querySelector(":root");
+let span = document.querySelector("slider round");
+
+span.addEventListener('click', () => {
+  event.preventDefault();
+  root.classList.toggle('dark');
+})

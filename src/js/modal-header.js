ceb4cfd9a8dbcddf23f5f66cd5ca15f5aa3409cb
Yulchikss <yuliapelia19@gmail.com>
2024-08-22 12:10:50 +0300

    feat: added greeting

--- src/js/modal-header.js
+++ src/js/modal-header.js
@@ -1,10 +1,35 @@
-const modal = document.querySelector('[data-model]')
-const modalOpen = document.querySelector('[data-open-modal]')
-const modalClose = document.querySelector('[data-modal-close]')
-const backdrop = document.querySelector('.backdrop')
+const modalClose = document.querySelector('.modal-header__close')
+const backdropHeader = document.querySelector('.backdrop-header')
+const headerGreet = document.querySelector('.header__greeting')
+const modalInput = document.querySelector('.modal-header__input')
+const saveBtn = document.querySelector('.modal-header__btn')
 
-console.log(modal);
-console.log(modalOpen);
 console.log(modalClose);
-console.log(backdrop);
+console.log(backdropHeader);
 
+saveBtn.addEventListener('click', () => {
+    if(modalInput.value !== '') {
+        headerGreet.textContent = `Вітаємо, ${modalInput.value}!`
+        toggleModal()
+    } else {
+        alert('Введіть ваше ім\'я')
+    }
+})
+
+const toggleModal = () => {
+    backdropHeader.classList.toggle('is-hidden')
+
+}
+
+
+modalClose.addEventListener('click', () => {
+    console.log('click');
+    toggleModal()
+})
+
+
+backdropHeader.addEventListener('click', (e) => {
+    if (e.target === backdropHeader) {
+        toggleModal();
+    }
+});
